a4051ced76082b953e01a4ef1ec30d26e3db3a71
Fernando Sproviero <fernandospr@gmail.com>
2021-08-09 17:58:49 -0300

    Refactoring lifemiles validator

--- app/src/main/java/com/github/fernandospr/validators/LifemilesIdValidator.kt
+++ app/src/main/java/com/github/fernandospr/validators/LifemilesIdValidator.kt
@@ -1,26 +1,21 @@
 package com.github.fernandospr.validators
 
 class LifemilesIdValidator {
+    private val validRange = 10..99999999999
 
     fun isValid(text: String): Boolean {
-        val number = text.toLongOrNull()
-        
-        if (number == null) {
-            return false
-        }
-        
-        if (number < 10 || number >= 100_000_000_000) {
-            return false
-        }
-
-        val a = number / 10
-        val b = number % 10
-        val result = a % 7 == b
-        if (!result) {
-            return false
-        }
-        
-        return true
+        val id = text.toLongOrNull()
+        return id != null && inValidRange(id) && isLastDigitValid(id)
     }
-    
+
+    private fun inValidRange(id: Long): Boolean {
+        return id in validRange
+    }
+
+    private fun isLastDigitValid(id: Long): Boolean {
+        val lastDigit = id % 10
+        val idExceptLastDigit = id / 10
+
+        return idExceptLastDigit % 7 == lastDigit
+    }
 }
