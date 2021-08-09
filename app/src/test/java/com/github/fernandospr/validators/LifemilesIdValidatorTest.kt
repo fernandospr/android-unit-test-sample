91a86c052446bb9d53c58fce88046d9c0de054e0
Fernando Sproviero <fernandospr@gmail.com>
2021-08-08 21:26:41 -0300

    Adding lifemiles validator tests

--- app/src/test/java/com/github/fernandospr/validators/LifemilesIdValidatorTest.kt
+++ app/src/test/java/com/github/fernandospr/validators/LifemilesIdValidatorTest.kt
@@ -12,4 +12,68 @@ class LifemilesIdValidatorTest {
 
         Assert.assertFalse(result)
     }
+
+    @Test
+    fun `id with char should not validate`() {
+        val validator = LifemilesIdValidator()
+
+        val result = validator.isValid("c")
+
+        Assert.assertFalse(result)
+    }
+
+    @Test
+    fun `id number with one digit should not validate`() {
+        val validator = LifemilesIdValidator()
+
+        val result = validator.isValid("1")
+
+        Assert.assertFalse(result)
+    }
+
+    @Test
+    fun `id number with twelve digits should not validate`() {
+        val validator = LifemilesIdValidator()
+
+        val result = validator.isValid("123456789012")
+
+        Assert.assertFalse(result)
+    }
+
+    @Test
+    fun `First valid id number should validate`() {
+        val validator = LifemilesIdValidator()
+
+        val result = validator.isValid("11")
+
+        Assert.assertTrue(result)
+    }
+
+    @Test
+    fun `Last valid id number should validate`() {
+        val validator = LifemilesIdValidator()
+
+        val result = validator.isValid("99999999993")
+
+        Assert.assertTrue(result)
+    }
+
+    @Test
+    fun `id number inside valid range and valid last digit should validate`() {
+        val validator = LifemilesIdValidator()
+
+        val result = validator.isValid("12342")
+
+        Assert.assertTrue(result)
+    }
+
+    @Test
+    fun `id number inside valid range and invalid last digit should not validate`() {
+        val validator = LifemilesIdValidator()
+
+        val result = validator.isValid("12343")
+
+        Assert.assertFalse(result)
+    }
+
 }
