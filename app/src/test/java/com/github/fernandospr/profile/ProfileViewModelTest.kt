001927626be5506a903955dc434289ee518ab0bd
Fernando Sproviero <fernandospr@gmail.com>
2021-08-09 22:49:18 -0300

    Moving common test logic to setup method

--- app/src/test/java/com/github/fernandospr/profile/ProfileViewModelTest.kt
+++ app/src/test/java/com/github/fernandospr/profile/ProfileViewModelTest.kt
@@ -1,6 +1,7 @@
 package com.github.fernandospr.profile
 
 import com.github.fernandospr.validators.LifemilesIdValidator
+import org.junit.Before
 import org.junit.Test
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.never
@@ -8,10 +9,18 @@ import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 
 class ProfileViewModelTest {
+
+    private lateinit var validatorMock: LifemilesIdValidator
+    private lateinit var repositoryMock: ProfileRepository
+
+    @Before
+    fun setup() {
+        validatorMock = mock()
+        repositoryMock = mock()
+    }
+
     @Test
     fun `Updating a lifemiles id should call save on repository when lifemiles validator successfully validates`() {
-        val validatorMock: LifemilesIdValidator = mock()
-        val repositoryMock: ProfileRepository = mock()
         whenever(validatorMock.isValid("1234")).thenReturn(true)
         val viewModel = ProfileViewModel(validatorMock, repositoryMock)
 
@@ -22,8 +31,6 @@ class ProfileViewModelTest {
 
     @Test
     fun `Updating a lifemiles id should not call save on repository when lifemiles validator fails to validate`() {
-        val validatorMock: LifemilesIdValidator = mock()
-        val repositoryMock: ProfileRepository = mock()
         whenever(validatorMock.isValid("1234")).thenReturn(false)
         val viewModel = ProfileViewModel(validatorMock, repositoryMock)
 
