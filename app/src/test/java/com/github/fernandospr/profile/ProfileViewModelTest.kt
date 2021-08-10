7742b99526a492f41019f1f0a29f6bd7612908b4
Fernando Sproviero <fernandospr@gmail.com>
2021-08-09 22:48:34 -0300

    Adding other profile viewmodel test

--- app/src/test/java/com/github/fernandospr/profile/ProfileViewModelTest.kt
+++ app/src/test/java/com/github/fernandospr/profile/ProfileViewModelTest.kt
@@ -3,6 +3,7 @@ package com.github.fernandospr.profile
 import com.github.fernandospr.validators.LifemilesIdValidator
 import org.junit.Test
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.never
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 
@@ -17,5 +18,17 @@ class ProfileViewModelTest {
         viewModel.update("1234")
 
         verify(repositoryMock).save("1234")
+    }
+
+    @Test
+    fun `Updating a lifemiles id should not call save on repository when lifemiles validator fails to validate`() {
+        val validatorMock: LifemilesIdValidator = mock()
+        val repositoryMock: ProfileRepository = mock()
+        whenever(validatorMock.isValid("1234")).thenReturn(false)
+        val viewModel = ProfileViewModel(validatorMock, repositoryMock)
+
+        viewModel.update("1234")
+
+        verify(repositoryMock, never()).save("1234")
     }
 }
